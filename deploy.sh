94f6438d830913df89f61b1ccf136563d98fd996
Daniel Cefram Ramirez <dan@daeram.com>
2019-07-29 07:28:37 +0800

    fix: sync with pub

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+echo -e "\033[0;32mBuilding updates through hugo...\033[0m"
 
 # Build the project.
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+
+echo -e "\033[0;32mDeploying updates to Github...\033[0m"
 
 # Go To Public folder
 cd public
@@ -16,6 +18,8 @@ if [ $# -eq 1 ]
   then msg="$1"
 fi
 git commit -m "$msg"
+
+echo -e "\033[0;32mPushing to master...\033[0m"
 
 # Push source and build repos.
 git push origin master
